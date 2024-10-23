5416d50ae2cef1abafc5c971ce5a70ee1d486bdf
vktornaj <victor2211812@gmail.com>
2024-10-23 00:43:22 +0000

    fix: user repository

--- src/domain/entities/user.rs
+++ src/domain/entities/user.rs
@@ -3,8 +3,9 @@ use serde::{Deserialize, Serialize};
 
 use crate::domain::value_objects::id::Id;
 
-#[derive(PartialEq, Clone, Debug)]
+#[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
 pub struct User {
+    #[serde(rename = "_id")]
     pub id: Id,
     pub email: String,
     pub created_at: DateTime<Utc>,
@@ -19,30 +20,5 @@ impl User {
             created_at: Utc::now(),
             updated_at: Utc::now(),
         }
-    }
-}
-
-impl Serialize for User {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut state = serializer.serialize_struct("User", 5)?;
-        // state.serialize_field("id", &self.id)?;
-        // state.serialize_field("email", &self.email)?;
-        // state.serialize_field("password", &self.password)?;
-        // state.serialize_field("created_at", &self.created_at)?;
-        // state.serialize_field("updated_at", &self.updated_at)?;
-        // state.end()
-        todo!()
-    }
-}
-
-impl<'de> Deserialize<'de> for User {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        todo!()
     }
 }
