f0eed902617fbe4b24d0051ebaf486206c6ed30a
vktornaj <victor2211812@gmail.com>
2024-10-24 15:46:59 +0000

    add: created_at to files list

--- src/domain/value_objects/file_info.rs
+++ src/domain/value_objects/file_info.rs
@@ -3,6 +3,7 @@ use serde::Serialize;
 
 #[derive(Debug, Serialize)]
 pub struct FileInfo {
+    pub id: String,
     pub name: String,
     pub file_type: String,
     pub created_at: Option<DateTime<Utc>>,
