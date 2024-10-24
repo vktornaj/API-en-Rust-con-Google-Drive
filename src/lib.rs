ae29bad589715ee5a1ecd8eb819cf93ae5575704
vktornaj <victor2211812@gmail.com>
2024-10-24 00:02:41 +0000

    fix: handle_google_callback

--- src/lib.rs
+++ src/lib.rs
@@ -20,24 +20,6 @@ use adapters::driving::http::{handlers, middlewares};
 pub async fn router() -> Router {
     let app_state = adapters::driving::http::state::AppState::new().await;
 
-    // run migrations
-    // run_migrations(&app_state.db_sql_pool).await;
-
-    let public_routes = Router::new();
-
-    let protected_routes = Router::new().route("/files", get(handlers::handler_get_list_files));
-
-    let api = Router::new()
-        .nest(
-            "/public",
-            public_routes
-                .layer(Extension(app_state.config.clone()))
-                .layer(middleware::from_fn(
-                    middlewares::authentication::auth_middleware,
-                )),
-        )
-        .nest("/protected", protected_routes);
-
     let service_builder = ServiceBuilder::new()
         .layer(middleware::from_fn(
             middlewares::logging::log_request_response,
@@ -46,6 +28,10 @@ pub async fn router() -> Router {
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
         )
+        .layer(Extension(app_state.config.clone()))
+        .layer(middleware::from_fn(
+            middlewares::authentication::auth_middleware,
+        ))
         .layer(
             CorsLayer::new()
                 .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
@@ -56,6 +42,23 @@ pub async fn router() -> Router {
                     "http://192.168.1.120".parse::<HeaderValue>().unwrap(),
                 ]),
         );
+
+    // Public routes
+    let public_routes = Router::new()
+        .route(
+            "/google-auth-url",
+            get(handlers::handler_get_google_auth_url),
+        )
+        .route("/callback", get(handlers::handler_handle_google_callback));
+
+    // Protected routes
+    let protected_routes =
+        Router::new().route("/list-files", get(handlers::handler_get_list_files));
+
+    // API
+    let api = Router::new()
+        .nest("/public", public_routes)
+        .nest("/protected", protected_routes);
 
     // Return a `Router`
     Router::new()
